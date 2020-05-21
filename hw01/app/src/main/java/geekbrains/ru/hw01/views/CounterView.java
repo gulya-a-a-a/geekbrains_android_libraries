fc0297e65833872b8bfc264ecd1fe1b10945d9fb
Александр Гуляев <aleksandrgulaev@iMac-Aleksandr.local>
2020-05-21 14:18:08 +0300

    Directories refactoring.

--- hw01/app/src/main/java/geekbrains/ru/hw01/views/CounterView.java
+++
@@ -1,8 +0,0 @@
-package geekbrains.ru.hw01.views;
-
-public interface CounterView {
-
-    void setCounterName(String name);
-
-    void setCounterValues(int value);
-}
--- hw01/app/src/main/java/geekbrains/ru/hw01/views/MainView.java
+++
@@ -1,9 +0,0 @@
-package geekbrains.ru.hw01.views;
-
-import java.util.List;
-
-import geekbrains.ru.hw01.models.Counter;
-
-public interface MainView {
-    void showCountersList(List<Counter> counters);
-}
