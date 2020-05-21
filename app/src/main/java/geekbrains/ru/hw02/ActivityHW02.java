3b609e55b0230d5637429d94a3eba2ededd48415
Aleksandr Gulyaev <adgulyaev@gmail.com>
2020-05-21 15:54:56 +0300

    Add publisher.

--- app/src/main/java/geekbrains/ru/hw02/ActivityHW02.java
+++ app/src/main/java/geekbrains/ru/hw02/ActivityHW02.java
@@ -2,38 +2,45 @@ package geekbrains.ru.hw02;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import geekbrains.ru.hw01.R;
 import io.reactivex.Observable;
+
 import io.reactivex.Observer;
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import io.reactivex.subjects.PublishSubject;
 
 public class ActivityHW02 extends AppCompatActivity {
 
-    private TextView mTextView;
-    private PublishSubject<String> mPublisherSubject;
+    private TextView mTextView, bottomText;
+    private PublishSubject<Integer> mPublisherSubject;
     private Button button1, button2;
+    private List<Integer> list1, list2;
+    private Observable<Integer> obs1, obs2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hw2);
 
+        publisherCreate();
+
         initControls();
 
         initEventBus();
 
-        publisherCreate();
     }
 
     private void initControls() {
@@ -58,45 +65,64 @@ public class ActivityHW02 extends AppCompatActivity {
 
         button1 = findViewById(R.id.button1);
         button1.setOnClickListener(v -> {
-//            mPublisherSubject.onNext(button1.getTag());
+            Observable<Integer> obs1 = getObserverable(list1).
+                    subscribeOn(Schedulers.newThread()).
+                    observeOn(AndroidSchedulers.mainThread());
+            mPublisherSubject.subscribe(getObserver(0));
+            obs1.subscribe(mPublisherSubject);
         });
-        Button button2 = findViewById(R.id.button2);
+        button2 = findViewById(R.id.button2);
+        bottomText = findViewById(R.id.textView1);
     }
 
 
     private void initEventBus() {
-        Observable<String> observable = Observable.fromArray("A", "F", "G");
-        observable.safeSubscribe(new Observer<String>() {
-            private static final String TAG = "Observer";
+        list1 = new ArrayList<>(10);
+        list2 = new ArrayList<>(10);
+        for (int i = 10; i < 20; i++) {
+            list1.add(i + 2);
+            list2.add(i * 2);
+        }
+
+    }
+
+    private Observable<Integer> getObserverable(List<Integer> list) {
+        return Observable.create(emitter -> {
+            for (int s : list) {
+                Thread.sleep(1000);
+                emitter.onNext(s);
+            }
+            emitter.onComplete();
+        });
+    }
+
+    private void publisherCreate() {
+        mPublisherSubject = PublishSubject.create();
+        mPublisherSubject.subscribe(System.out::println);
+    }
+
+    private Observer<Integer> getObserver(int id) {
+        return new Observer<Integer>() {
             @Override
             public void onSubscribe(Disposable d) {
-                Log.d(TAG, "onSubscribe = " + d.toString());
+
             }
 
             @Override
-            public void onNext(String s) {
-                Log.d(TAG, "onNext = " + s);
+            public void onNext(Integer integer) {
+                bottomText.setText(String.format("Integer: %d", integer));
             }
 
             @Override
             public void onError(Throwable e) {
-                Log.d(TAG, "onError = " + e.getMessage());
+
             }
 
             @Override
             public void onComplete() {
-                Log.d(TAG, "onComplete");
+
             }
-        });
+        };
     }
-
-    private void publisherCreate(){
-        mPublisherSubject = PublishSubject.create();
-        mPublisherSubject.subscribe(System.out::println);
-        mPublisherSubject.onNext("D");
-        mPublisherSubject.onNext("T");
-
-    }
-
 
 }
