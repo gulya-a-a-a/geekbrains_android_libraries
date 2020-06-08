5a585684dcd600d91b4f40725d1c945fb27f2de1
Aleksandr Gulyaev <adgulyaev@gmail.com>
2020-06-08 12:28:43 +0300

    Init branch.

---
+++ app/src/main/java/geekbrains/ru/hw06/ActivityHW06.java
@@ -0,0 +1,17 @@
+package geekbrains.ru.hw06;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+import geekbrains.ru.hw01.R;
+import geekbrains.ru.hw04.activity.BaseActivity;
+
+public class ActivityHW06 extends BaseActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hw06);
+    }
+}
